133f5da343809f637ee0c16d55d5c5e75d6824bf
Giorgos Flouris <giorgos.flouris.98@gmail.com>
2021-03-08 19:43:06 +0200

    fix: rules and grammaer

--- utilities.c
+++ utilities.c
@@ -26,10 +26,6 @@ void insertID(char* name, int scope, int line) {
         return;
     }
 
-    if(comparelibfunc(name) == -1) {
-        return;
-    }
-
     if(getEntryType(temp) == "USERFUNC" && temp -> isActive == 1) {
         yyerror("A function has taken already that name!");
         return;
@@ -47,6 +43,63 @@ void insertID(char* name, int scope, int line) {
         new_entry -> varVal = new_var;
         new_entry -> funcVal = NULL;
 
+        scope == 0? (new_entry -> type = GLOBAL): (new_entry -> type = LOCAL);
+
+        insertEntry(new_entry);
+        return;
+    }
+
+    return;
+}
+
+void insertLocalID(char* name, int scope, int line) {
+    SymbolTableEntry *temp;
+    SymbolTableEntry *new_entry;
+    Variable *new_var;
+
+    temp = lookupEverything(name);
+
+    if(temp == NULL) {
+        new_entry = (SymbolTableEntry*)malloc(sizeof(SymbolTableEntry));
+        new_var = (Variable*)malloc(sizeof(Variable));
+
+
+        new_var -> name = name;
+        new_var -> scope = scope;
+        new_var -> line = line;
+
+        new_entry -> isActive = 1;
+        new_entry -> varVal = new_var;
+        new_entry -> funcVal = NULL;
+        
+        scope == 0? (new_entry -> type = GLOBAL): (new_entry -> type = LOCAL);
+
+        insertEntry(new_entry);
+        return;
+    }
+
+    if(temp -> type == USERFUNC && temp -> isActive == 1) {
+        yyerror("A user function with that name already exists!\n");
+        return;
+    }   
+
+    if(comparelibfunc(name) == -1) {
+        return;
+    }
+
+    if(temp -> varVal -> scope > scope) {
+        new_entry = (SymbolTableEntry*)malloc(sizeof(SymbolTableEntry));
+        new_var = (Variable*)malloc(sizeof(Variable));
+
+
+        new_var -> name = name;
+        new_var -> scope = scope;
+        new_var -> line = line;
+
+        new_entry -> isActive = 1;
+        new_entry -> varVal = new_var;
+        new_entry -> funcVal = NULL;
+        
         scope == 0? (new_entry -> type = GLOBAL): (new_entry -> type = LOCAL);
 
         insertEntry(new_entry);
--- utilities.h
+++ utilities.h
@@ -1,3 +1,4 @@
 #include "SymbolTable.h"
 
 void insertID(char* name, int scope, int line);
+void insertLocalID(char* name, int scope, int line);
